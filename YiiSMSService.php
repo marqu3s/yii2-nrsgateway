21ac227eeea22d1fa817da62d4b988efbeb815e7
João Marques <joao@jjmf.com>
2016-08-01 15:00:51 -0300

    Update YiiSMSService.php

--- YiiSMSService.php
+++ YiiSMSService.php
@@ -6,15 +6,14 @@
  * Time: 13:54
  */
 
-namespace marqu3s\yii2nrsgateway;
+namespace marqu3s\nrsgateway;
 
 use yii\base\Component;
-use marqu3s\nrsgateway\SMSService;
 
 class YiiSMSService extends Component
 {
     /** @var SMSService */
-    private $_nrs = null;
+    private $nrs = null;
 
     public $username;
     public $password;
@@ -22,8 +21,8 @@ class YiiSMSService extends Component
 
     public function init()
     {
-        if ($this->_nrs === null) {
-            $this->_nrs = new SMSService($this->username, $this->password);
+        if ($this->nrs === null) {
+            $this->nrs = new SMSService($this->username, $this->password);
         }
     }
 }
